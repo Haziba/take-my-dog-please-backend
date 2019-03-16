e461b01c12735aef74bad0a603db7d2e594faacf
Haziba <harry.boyes@gmail.com>
2019-03-16 16:15:17 +0000

    Publish in deploy

--- deploy.sh
+++ deploy.sh
@@ -1,2 +1,3 @@
+dotnet publish -o ./publish
 aws cloudformation package --template-file template.yaml --s3-bucket take-my-dog-please --output-template-file template-output.yaml --region eu-west-1
 aws cloudformation deploy --template-file template-output.yaml --stack-name take-my-dog-please --capabilities CAPABILITY_IAM
